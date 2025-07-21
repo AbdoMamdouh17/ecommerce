b6294d40b0bad99401e4418ec0aa4c00311f6859
Abdo Mamdouh <abdomamdouh815@gmail.com>
2025-07-21 17:13:56 +0300

    Remove duplicate folder

--- ecommerce-main/ecommerce-main/DB/connection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async ()=>{
-    return await mongoose.connect(process.env.CONNECTION_URL).then(()=>{
-        console.log("DB connected Successfully")
-    }).catch(()=>{
-        console.log("failed to connected DB")
-    })
-}
